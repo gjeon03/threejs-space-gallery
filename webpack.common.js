8dfda3a4075103bb7e26cf5d27fdbe55c286c960
gyeongyeon-jeon <gy.jeon@purple.io>
2023-10-28 04:02:37 +0900

    fix: webpack 재설정

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const webpackMode = process.env.NODE_ENV || "development";
 
 module.exports = {
-  entry: "./src/client.ts",
+  mode: webpackMode,
+  entry: {
+    main: "./src/client.ts",
+  },
   module: {
     rules: [
       {
@@ -14,8 +23,46 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
+  optimization: {
+    minimizer:
+      webpackMode === "production"
+        ? [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+          ]
+        : [],
+    splitChunks: {
+      chunks: "all",
+    },
   },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/client.css", to: "./client.css" },
+        { from: "./src/images", to: "./images" },
+        // { from: "./src/models", to: "./models" },
+        // { from: "./src/sounds", to: "./sounds" }
+      ],
+    }),
+  ],
 };
